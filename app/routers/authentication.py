c5f83a69c872b995468f43943aa00fff7e507a5c
idrisabkar <idrisabkar2018@gmail.com>
2023-02-19 20:00:03 +0300

    secforms

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter,Depends,HTTPException,status,Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from app.schemas import LoginCridentials
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -8,8 +9,8 @@ from app import models,oauth2
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login",status_code= status.HTTP_200_OK)
-def login(credentials : LoginCridentials,db : Session = Depends(get_db)):
-    user = db.query(models.Users).filter(models.Users.email == credentials.email).first()
+def login(credentials : OAuth2PasswordRequestForm,db : Session = Depends(get_db)):
+    user = db.query(models.Users).filter(models.Users.email == credentials.username).first()
     if not user or verify(c_password=credentials.password,h_password=user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Cridentials")
     else :
