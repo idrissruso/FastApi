7cbfcf0922b56ab2e20229bc8d1098ac9c1a7057
idrisabkar <idrisabkar2018@gmail.com>
2023-03-01 22:11:17 +0300

    cm

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app import database
 from app.settings import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
